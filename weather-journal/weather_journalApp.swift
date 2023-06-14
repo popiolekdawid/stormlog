0b408546eca57973bbcc1be5ef7a72e78233921e
Dawid Popiołek <dawidpopiolek@protonmail.com>
2023-06-14 23:08:23 +0200

    Initial Commit

---
+++ weather-journal/weather_journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_journalApp.swift
+//  weather-journal
+//
+//  Created by Dawid Popiołek on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
